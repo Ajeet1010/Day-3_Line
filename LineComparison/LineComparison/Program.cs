6183a0963da6b1c4c56fbcfa570037d2fcac2368
ajeet1010 <ajeetpal101999@gmail.com>
2022-12-07 06:43:58 +0530

    Calculating the length of line

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public class Length
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose the option to perform the operation:-  1) Calculate length \n" +
+                     "Enter the no to operate: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Line line = new Line();
+                        line.Calculate();
+                        break;
+                }
+            }
+        }
+    }
+}
